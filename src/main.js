d8f59b2c1194e861b7604f86c7b3b986f4f826e2
Yurii <voronich97@gmail.com>
2025-07-28 12:21:19 +0300

    test

--- src/main.js
+++ src/main.js
@@ -1,26 +1,23 @@
+import axios from "axios";
 const formEl = document.querySelector(".form");
-
-
 
 const onSubmit = async (e) => {
   e.preventDefault();
   const { name, email, message } = e.currentTarget.elements;
-  const formData = ` заявка на регистрацію на івент 
-  ім'я: ${name.value.trim()},
-    email: ${email.value.trim()},
-    message: ${message.value.trim()}`;
-  console.log(formData);
-  const response = await fetch(
-    `https://api.telegram.org/bot/${botToken}/sendMessage`,
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        chat_id: chatID,
-        text: formData,
-      }),
-    }
-  );
+  try {
+    const response = await axios.post(
+      "https://test-server-fortg.onrender.com/send-message",
+      {
+        name: name.value.trim(),
+        email: email.value.trim(),
+        message: message.value.trim(),
+      }
+    );
+
+    console.log("Відповідь сервера:", response.data);
+  } catch (error) {
+    console.error("Помилка при надсиланні:", error);
+  }
 };
 
 formEl.addEventListener("submit", onSubmit);
